edc8a8754c9860fb7ef1841024b1d1e5972b30c6
PauloK3tchup <passandoalimpo13@gmail.com>
2023-03-07 14:56:46 -0300

    criamento da editeira

--- livraria/models.py
+++ livraria/models.py
@@ -6,3 +6,11 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.descricao
+
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+    site = models.URLField(null=True, blank=True)  # campo opcional
+
+    def __str__(self):
+        return self.nome
