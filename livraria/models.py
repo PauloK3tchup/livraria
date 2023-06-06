56aae957257ca767b18bf53b3f92a6b17a5cc5ee
PauloK3tchup <passandoalimpo13@gmail.com>
2023-06-06 15:02:42 -0300

    ManyToMany

--- livraria/models.py
+++ livraria/models.py
@@ -31,17 +31,13 @@ class Livro(models.Model):
     quantidade = models.IntegerField(default=0,  null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
 
-    categoria = models.ForeignKey(
-        Categoria, on_delete=models.PROTECT, related_name="livros"
-    )
+    categorias = models.ManyToManyField(Categoria, related_name="livros")
 
     editora = models.ForeignKey(
         Editora, on_delete=models.PROTECT, related_name="livros"
     )
     
-    autor = models.ForeignKey(
-        Autor, on_delete=models.PROTECT, related_name="livros"
-    )
+    autores = models.ManyToManyField(Autor, related_name="livros")
     
     def __str__(self):
         return f"{self.titulo} ({self.quantidade})"
