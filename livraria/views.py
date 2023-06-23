aa02d043af4f30928d7450eb81fc0085f1848a12
PauloK3tchup <passandoalimpo13@gmail.com>
2023-06-23 14:30:38 -0300

    Aula 12: Permissões no settings

--- livraria/views.py
+++ livraria/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+#from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from livraria.models import Categoria, Editora, Autor, Livro
@@ -8,6 +8,7 @@ from livraria.serializers import CategoriaSerializer, EditoraSerializer, AutorSe
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+#    permission_classes = [IsAuthenticated]
     
 class EditoraViewSet(ModelViewSet):
     queryset = Editora.objects.all()
