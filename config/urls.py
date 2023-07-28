9b319d2c3543ef78cd6a73217932d22f6a32c6f5
PauloK3tchup <passandoalimpo13@gmail.com>
2023-07-28 14:38:25 -0300

    Imagem da capa

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 from usuario.router import router as usuario_router
+from uploader.router import router as uploader_router
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+
 
 from rest_framework.routers import DefaultRouter
 
@@ -22,4 +26,7 @@ urlpatterns = [
     path("", include(router.urls)),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/media/", include(uploader_router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
