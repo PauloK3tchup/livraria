620a878fb63aec079d4131dfaae7675af2d4d95c
PauloK3tchup <passandoalimpo13@gmail.com>
2023-08-11 15:09:54 -0300

    Pequena alteração

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import Autor, Categoria, Editora, Livro, Compra
+from .models import Autor, Categoria, Editora, Livro, Compra, ItensCompra
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Autor)
 admin.site.register(Livro)
-admin.site.register(Compra)
+
+class ItensCompraInline(admin.TabularInline):
+    model = ItensCompra
+    
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    inlines = [ItensCompraInline]
--- livraria/models/compra.py
+++ livraria/models/compra.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from usuario.models import Usuario
+from .livro import Livro
 
 class Compra(models.Model):
     class StatusCompra(models.IntegerChoices):
@@ -12,3 +13,8 @@ class Compra(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='compras')
     status = models.IntegerField(choices=StatusCompra.choices, default=StatusCompra.CARRINHO)
     # data = models.DateTimeField(auto_now_add=True)
+    
+class ItensCompra(models.Model):
+    compra = models.ForeignKey(Compra, on_delete=models.CASCADE, related_name="itens")
+    livro = models.ForeignKey(Livro, on_delete=models.PROTECT, related_name="+")
+    quantidade = models.IntegerField(default=1)
