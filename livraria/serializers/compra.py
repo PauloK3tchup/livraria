ed150ca8ebda23bdb5f4ef1fb7a9cabbd2549c57
PauloK3tchup <passandoalimpo13@gmail.com>
2023-09-01 14:31:31 -0300

    Preco do livro

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -1,11 +1,23 @@
-from rest_framework.serializers import ModelSerializer, CharField
+from rest_framework.serializers import ModelSerializer, CharField, SerializerMethodField
 
-from livraria.models import Compra
+from livraria.models import Compra, ItensCompra
+        
+class ItensCompraSerializer(ModelSerializer):
+    total = SerializerMethodField()
+    
+    def get_total(self, obj):
+        return obj.livro.preco * obj.quantidade
+
+    class Meta:
+        model = ItensCompra
+        fields = ["livro", "quantidade"]
+        depth = 1
+        
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source="usuario.email", read_only=True)
     status = CharField(source="get_status_display", read_only=True)
-
+    itens = ItensCompraSerializer(many=True, read_only=True)
 
     class Meta:
         model = Compra
